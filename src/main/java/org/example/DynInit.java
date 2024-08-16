fb1156bc01051bdb6c4c46d092c2dd10f998ea37
Razmik2001 <razmik33651@gmail.com>
2024-08-16 14:40:31 +0400

    1

--- src/main/java/org/example/DynInit.java
+++ src/main/java/org/example/DynInit.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class DynInit {
+    public static void main(String[] args)
+    {
+        double a =3.0,b =4.0;
+        System.out.println(Math.sqrt(a*a+b*b));
+    }
 }
--- src/main/java/org/example/ScopeError.java
+++ src/main/java/org/example/ScopeError.java
@@ -1,4 +1,11 @@
 package org.example;
 
 public class ScopeError {
+//    public static void main(String[] args) {
+//        int val1 = 12;
+//        {
+//            int val2 = 21;
+//        }
+//        System.out.println(val2);
+//    }
 }
